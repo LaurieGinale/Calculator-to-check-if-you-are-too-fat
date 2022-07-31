4f0c06a4552ab9c0a5163379384f05afa703e651
LaurieGinale <106886596+LaurieGinale@users.noreply.github.com>
2022-07-31 15:16:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+/* Definition of variables */
+const bmiResults = document.querySelector('.bmi');
+const bmiAnalysis = document.querySelector('.bmi-analysis');
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+const bmiData = [
+    {name: "Tu es trop maigre.", color: "red", range: [0, 18.5]},
+    {name: "Tu as une corpulence normale.", color: "green", range: [18.5, 25]},
+    {name: "Tu es en surpoids.", color: "red", range: [25, 30]},
+    {name: "Tu es obèse.", color: "red", range: [30, 35]},
+    {name: "Tu es très obèse.", color: "red", range: 35}
+];
+
+/* Form submission */
+
+form.addEventListener('submit', handleForm);
+
+/* Handle form */
+
+function handleForm(e) {
+    e.preventDefault();
+    calculateBMI();
+}
+
+/* BMI calculation 
+ IMC = weight(kg) / height**2 en mètres */
+
+ function calculateBMI() {
+     const height = inputs[0].value;
+     const weight = inputs[1].value;
+
+     if(!height || !weight || height <= 0 || weight <= 0) { 
+         handleError();
+         return;
+     }
+
+     const imc = (weight / Math.pow(height/100, 2)).toFixed(1);
+     showResult(imc);
+ }
+
+/* Error function */
+
+function handleErro() {
+    bmiResults.textContent = "Oups";
+    bmiResults.style.color = "inherit";  
+    bmiAnalysis.textContent = "Veuillez remplir les deux champs svp";
+}
+
+/* Show results */
+
+function showResult(imc) {
+    const rank = bmiData.find(data => {
+        if(imc >= data.range[0] && imc < data.range[1]) return data;
+        else if(typeof data.range === "number" && imc >= data.range) return data;
+        })
+
+    bmiResults.textContent = imc;
+    bmiResults.style.color = `${rank.color}`;
+    bmiAnalysis.textContent = `${rank.name}`;
+    }
